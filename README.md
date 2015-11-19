10faf366dc137e036d38e47fed6350c454d86c4d
Simon Pigot <Simon.Pigot@csiro.au>
2015-11-19 16:09:43 +1100

    Add initial layout

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # iso19139.mcp
-ISO19115/19139 Marine Community Profile
+
+ISO19115/19139 Marine Community Profile schema plugin
+
+This is the ISO19115/19139 schema plugin for GeoNetwork 3.x or greater version.
+
+
---
+++ src/main/java/org/fao/geonet/schema/iso19139mcp/ISO19139MCPNamespaces.java
@@ -0,0 +1,12 @@
+package org.fao.geonet.schema.iso19139mcp;
+
+import org.jdom.Namespace;
+
+/**
+ * Namespaces for iso19139.mcp metadata standard.
+ */
+public class ISO19139MCPNamespaces {
+    public static final Namespace MCP =
+            Namespace.getNamespace("mcp", "http://bluenet3.antcrc.utas.edu.au/mcp");
+	  /* rest of the namespaces are shared with ISO19139Namespaces */
+}
