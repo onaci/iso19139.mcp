d6e512be879e35ef10a4a1d3cbc8c41295cedbaa
Simon Pigot <sppigot@gmail.com>
2017-08-03 12:26:08 +1000

    Use more info from base ISO19139 schema

--- src/main/java/org/fao/geonet/schema/iso19139mcp/ISO19139MCPNamespaces.java
+++ src/main/java/org/fao/geonet/schema/iso19139mcp/ISO19139MCPNamespaces.java
@@ -8,12 +8,4 @@ import org.jdom.Namespace;
 public class ISO19139MCPNamespaces {
     public static final Namespace MCP =
             Namespace.getNamespace("mcp", "http://bluenet3.antcrc.utas.edu.au/mcp");
-    public static final Namespace GCO =
-            Namespace.getNamespace("gco", "http://www.isotc211.org/2005/gco");
-    public static final Namespace SRV =
-            Namespace.getNamespace("srv", "http://www.isotc211.org/2005/srv");
-    public static final Namespace GMD =
-            Namespace.getNamespace("gmd", "http://www.isotc211.org/2005/gmd");
-    public static final Namespace GMX =
-            Namespace.getNamespace("gmx", "http://www.isotc211.org/2005/gmx");
 }
